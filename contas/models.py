404e7560b0f8780f68d7e97dca22b52055d2847d
Taisa <taisa.tavares93@gmail.com>
2022-07-08 21:25:36 -0300

    add estilo

--- contas/models.py
+++ contas/models.py
@@ -6,6 +6,12 @@ class Recebimento(models.Model):
     descricao = models.TextField()
     valor = models.DecimalField(max_digits=10, decimal_places=2)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.titulo} - {self.valor}'
+
 class Despesas(models.Model):
     titulo = models.CharField(max_length=255)
     descricao = models.TextField()
@@ -16,4 +22,4 @@ class Despesas(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.titulo
+        return f'{self.titulo} - {self.valor}'
