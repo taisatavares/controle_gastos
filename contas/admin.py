896d36b1de175e5ac98990b1aa21f1f350c92116
Taisa <taisa.tavares93@gmail.com>
2022-07-13 20:29:41 -0300

    estilizando template

--- contas/admin.py
+++ contas/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Recebimento
 from .models import Despesas
+from .models import Resultado
 
 admin.site.register(Recebimento)
 admin.site.register(Despesas)
+admin.site.register(Resultado)
+
--- contas/models.py
+++ contas/models.py
@@ -23,3 +23,14 @@ class Despesas(models.Model):
 
     def __str__(self):
         return f'{self.titulo} - {self.valor}'
+
+class Resultado(models.Model):
+    titulo = models.CharField(max_length=255)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.titulo} - {self.valor}'
