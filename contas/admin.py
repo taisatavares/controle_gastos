65783f24e418674a7e1492a73c639296d39e6ec7
Taisa <taisa.tavares93@gmail.com>
2022-07-25 18:57:38 -0300

    adicionando valores

--- contas/admin.py
+++ contas/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from .models import Conta
 from .models import Despesa
+from .models import Resultado
 
 admin.site.register(Conta)
 admin.site.register(Despesa)
-
+admin.site.register(Resultado)
--- contas/views.py
+++ contas/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Conta
 from .models import Despesa
+from .models import Resultado
 
 def contaList(request):
     contas = Conta.objects.all()
     despesas = Despesa.objects.all()
-    return render(request, 'contas/list.html', {'contas':contas, 'despesas':despesas})
+    resultados = Resultado.objects.all()
+    return render(request, 'contas/list.html', {'contas':contas, 'despesas':despesas, 'resultados':resultados})
 
 
 def helloWorld(request):
