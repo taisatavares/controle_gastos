3dd95d8c6f09f7303632408f098914c5d5139ace
Taisa <taisa.tavares93@gmail.com>
2022-07-31 12:50:16 -0300

    puxando dados do admin

--- contas/urls.py
+++ contas/urls.py
@@ -4,5 +4,7 @@ from .import views
 urlpatterns = [
     path('helloworld/', views.helloWorld),
     path('', views.contaList, name='conta-list'),
+    path('conta/<int:id>', views.contaView, name="conta-view"),
+    path('despesa/<int:id>', views.despesaView, name="despesa-view"),
     path('yourname/<str:name>', views.yourName, name='your-name'),
 ]
--- contas/views.py
+++ contas/views.py
@@ -1,18 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Conta
 from .models import Despesa
 from .models import Resultado
+from decimal import Decimal
+
 
 def contaList(request):
     contas = Conta.objects.all()
     despesas = Despesa.objects.all()
-    resultados = Resultado.objects.all()
-    return render(request, 'contas/list.html', {'contas':contas, 'despesas':despesas, 'resultados':resultados})
+    resultado = calcula_resultado(contas, despesas)
+    return render(request, 'contas/list.html', {'contas': contas, 'despesas': despesas, 'resultado_final': resultado})
+
+
+def calcula_resultado(contas: list[Conta], despesas: list[Despesa]):
+    receita_total = Decimal()
+    for conta in contas:
+        receita_total += conta.valor
+
+    for despesa in despesas:
+        receita_total -= despesa.valor
+
+    return receita_total
+
+
+def contaView(request, id):
+    conta = get_object_or_404(Conta, pk=id)
+    return render(request, 'contas/conta.html', {'conta': conta})
+
+def despesaView(request, id):
+    despesa = get_object_or_404(Despesa, pk=id)
+    return render(request, 'contas/despesa.html', {'despesa': despesa})
 
 
 def helloWorld(request):
     return HttpResponse('Hello World!')
 
+
 def yourName(request, name):
-    return render(request, 'contas/yourname.html', {'name':name})
+    return render(request, 'contas/yourname.html', {'name': name})
